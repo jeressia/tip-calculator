d8b6d5f1ba59043be83dc999b582e7b9d852f6e4
Jeressia Williamson <jeressiajaytv@gmail.com>
2022-04-24 21:38:05 -0500

    working on math

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
-let billInput = document.getElementById('bill-input');
-let numberOfPeopleInput = document.getElementById('people-count-input');
-let fivePercentTip = document.getElementById('5');
-let tenPercentTip = document.getElementById('10');
-let fifteenPercentTip = document.getElementById('15');
-let twentyfivePercentTip = document.getElementById('25');
-let fiftyPercentTip = document.getElementById('50');
-let customTip = document.getElementById('custom');
+const billInput = document.getElementById('bill-input');
+const numberOfPeopleInput = document.getElementById('people-count-input');
+const fivePercentTip = document.getElementById('5');
+const tenPercentTip = document.getElementById('10');
+const fifteenPercentTip = document.getElementById('15');
+const twentyfivePercentTip = document.getElementById('25');
+const fiftyPercentTip = document.getElementById('50');
+const customTip = document.getElementById('custom');
+const resetBtn = document.getElementById('reset-btn');
 
-
-const intialBill = () => console.log(billInput.value);
+const intialBill = billInput.value;
+const userInput = () => console.log(billInput.value);
 const totalPeople = () => console.log(numberOfPeopleInput.value)
 const customTipInput = () => console.log(customTip.value)
-const tipPercentage = (tip) => console.log(tip)
+const tipPercentage = (tip) => {
+    const tipAsNumber = Number(tip);
+    console.log(intialBill, 'initial number')
+    console.log(tipAsNumber, 'tip')
+    console.log(intialBill * tipAsNumber);
+}
 
-billInput.addEventListener('input', intialBill);
+const eventListeners = () => {
+billInput.addEventListener('input', userInput);
 numberOfPeopleInput.addEventListener('input', totalPeople);
-
 customTip.addEventListener('input', customTipInput);
-fivePercentTip.addEventListener('click', tipPercentage(fivePercentTip.value));
-tenPercentTip.addEventListener('click', tipPercentage(0.10));
-fifteenPercentTip.addEventListener('click', tipPercentage(0.15));
-twentyfivePercentTip.addEventListener('click', tipPercentage(0.25));
-fiftyPercentTip.addEventListener('click', tipPercentage(0.50));
+}
+
+const selectedTip = (tipSelected) => {
+    if (!tipSelected.classList.contains('active')) {
+        tipSelected.classList.add('active')
+    } else if (tipSelected.classList.contains('active')) {
+        tipSelected.classList.remove('active')
+    }
+}
+
+const resetFields = () => {
+    billInput.value = 0;
+    numberOfPeopleInput.value = 0;
+    customTip.value = 0;
+}
+
+const init = () => {
+    eventListeners();
+}
+
+init();
